ee28ef9a893e378dd0740b2227152fb1ba620797
Basonee <33735777+Basonee@users.noreply.github.com>
2017-11-17 00:24:37 +0100

    Update menu.h

--- menu.h
+++ menu.h
@@ -3,7 +3,7 @@
 #include <stdbool.h>
  
 
-  void tri_bulle(int* tableau)
+  int *croissant(int* tableau)
 {
 
 
@@ -14,36 +14,40 @@
     while (permutation) {
         permutation = false;
         passage ++; 
-        for (cas=0;cas<20-passage;cas++) {
+        for (cas=0;tableau[cas +1] != NULL;cas++) {
             if (tableau[cas]>tableau[cas+1]){
                 permutation = true;
                 int temp = tableau[cas];
                 tableau[cas] = tableau[cas+1];
-                tableau[cas] = temp;
-                printf("test");     
+                tableau[cas+1] = temp;
+                
             }
-          
+    
         }
        
     }
-
+    return (tableau);
 }
 
 int main(int argc, char *argv[]) {
-
-  int cas = 0;
-  int* tableau[cas];
-  *tableau[0] = 97;
-  *tableau[1] = 42;
-  *tableau[2] = 89;
-  *tableau[3] = 21;
-  *tableau[4] = 14;
-  *tableau[5] = 36;
-  *tableau[6] = 58;
-  *tableau[7] = 65;
-  *tableau[8] = 73;
-
- 
+	int print = 0;
+  int cas = 9;
+  int tableau[cas];
+  tableau[0] = 97;
+  tableau[1] = 42;
+  tableau[2] = 89;
+  tableau[3] = 21;
+  tableau[4] = 14;
+  tableau[5] = 36;
+  tableau[6] = 58;
+  tableau[7] = 65;
+  tableau[8] = 73;
+  
+	int *result = croissant(tableau);
+	for (print = 0; print<cas;print++) {
+	
+		printf("%d\n", result[print]);
+}
 return 0;
 }
 
